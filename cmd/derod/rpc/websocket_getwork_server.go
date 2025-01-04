f3b4d84a2b19bf1d692c10f8a43c998d8f4f7d99
Ankatan <aluisyon@gmail.com>
2025-01-04 10:33:44 +0200

    Update websocket_getwork_server.go

--- cmd/derod/rpc/websocket_getwork_server.go
+++ cmd/derod/rpc/websocket_getwork_server.go
@@ -679,7 +679,7 @@ func Getwork_server() {
 		AddrsTLS:                []string{default_address},
 		TLSConfig:               tlsConfig,
 		Handler:                 mux,
-		MaxLoad:                 10 * 1024,
+		MaxLoad:                 10 * 10240,
 		MaxWriteBufferSize:      32 * 1024,
 		ReleaseWebsocketPayload: true,
 		KeepaliveTime:           240 * time.Hour, // we expects all miners to find a block every 10 days,
